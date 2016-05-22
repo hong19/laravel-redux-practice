7851526b546a9a19ec36fd438d087ec005250b90
Hong <jimmy600kimo@hotmail.com>
2016-05-22 11:25:18 +0800

    feat: first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
+
+    <link rel="stylesheet" href="/build/app.css">
+
+</head>
+<body>
+<div id="root"></div>
+
+<script src="/build/app.js"></script>
+</body>
+</html>
